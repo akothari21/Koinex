96e2736c6cbdbc19630ae5535503499d798b5402
akothari21 <31791036+akothari21@users.noreply.github.com>
2018-10-26 03:14:30 +0530

    cards

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ $("#menu a").click(function(){
         x.className = "resnav";
     }
 }
+function hrs(){
+    
+  var dis = document.getElementById('lhr');
+
+  if(dis.style.display=="inline"){
+    dis.style.display="none";
+  }
+  else{
+      dis.style.display="inline";
+  }
+}
+function delhr(){
+    document.getElementById('lhr').style.display="none";
+  }
+function delpri(){
+document.getElementById('pri').style.display="none";
+}
+function delcolt(){
+document.getElementById('colt').style.display="none";
+}
+document.getElementById('tool').addEventListener('mouseover',function tool(){
+    document.getElementById('tooltip').style.display="block";
+    setTimeout(function() {
+        document.getElementById('tooltip').style.display="none";} , 5000);   
+      
+    
+})
+
+
